8998461aea5c6236a0cc05d6819912528735d8ac
Anlin <anlin.g@ecsfin.com>
2023-08-14 09:06:40 +0530

    First Commit...

---
+++ Assessment/src/main/java/Task/Assessment/App.java
@@ -0,0 +1,13 @@
+package Task.Assessment;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Assessment!" );
+    }
+}
